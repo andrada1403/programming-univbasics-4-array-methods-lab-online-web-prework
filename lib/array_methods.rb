28c8289fca88c23057532324d543b35c8d3b9efc
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-09-24 20:11:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
 element1="wow"
-array.include?elem
 element2="Tardis"
+array.include?element
 end
 
 def using_sort(array)
