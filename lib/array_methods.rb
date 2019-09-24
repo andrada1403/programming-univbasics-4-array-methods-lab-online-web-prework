ef83e2698aa8a40a4c22ecbe7fde95a253f6c888
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-09-24 20:13:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-element1="wow"
-element2="Tardis"
+element="wow"
+element="Tardis"
 array.include?(element)
 end
 
